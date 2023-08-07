105bb70b60ff0a2338722298ae7a148fa7bad454
sathreyashankar <athreya036@gmail.com>
2023-08-07 23:58:10 +0530

    initial comit

---
+++ TextScannerApp/app/src/main/java/com/example/textscannerapp/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.textscannerapp;
+
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_CODE = 100;
+
+    private Button buttonCapture, buttonCopy;
+    private TextView textViewData;
+
+    private Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonCapture = findViewById(R.id.button_capture);
+        buttonCopy = findViewById(R.id.button_copy);
+        textViewData = findViewById(R.id.text_data);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, REQUEST_CAMERA_CODE);
+        } else {
+            initializeCamera();
+        }
+
+        buttonCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(MainActivity.this);
+            }
+        });
+
+        buttonCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scannedText = textViewData.getText().toString();
+                copyToClipboard(scannedText);
+            }
+        });
+    }
+
+    private void initializeCamera() {
+        buttonCapture.setEnabled(true);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                if (result != null) {
+                    Uri resultUri = result.getUri();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                        getTextFromImage(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Crop image result is null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    private void getTextFromImage(Bitmap bitmap) {
+        TextRecognizer recognizer = new TextRecognizer.Builder(this).build();
+        if (!recognizer.isOperational()) {
+            Toast.makeText(MainActivity.this, "Error Occurred!", Toast.LENGTH_SHORT).show();
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> textBlockSparseArray = recognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < textBlockSparseArray.size(); i++) {
+                TextBlock textBlock = textBlockSparseArray.valueAt(i);
+                stringBuilder.append(textBlock.getValue());
+                stringBuilder.append("\n");
+            }
+            textViewData.setText(stringBuilder.toString());
+            buttonCapture.setText("Retake");
+            buttonCopy.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void copyToClipboard(String text) {
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied Data", text);
+        clipboardManager.setPrimaryClip(clip);
+        Toast.makeText(MainActivity.this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CAMERA_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initializeCamera();
+            } else {
+                Toast.makeText(MainActivity.this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
